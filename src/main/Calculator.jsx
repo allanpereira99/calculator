d5e7cb135a41c55a3357fa0f712b4967bfc2f7b6
allanpereira99 <allan.pereira.dev@gmail.com>
2022-05-02 21:45:14 -0300

    add calculator logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 import "./Calculator.css";
 import Button from "../components/Button";
 import Display from "../components/Display";
+const initialState = {
+  displaValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
+  state = { ...initialState };
   constructor(props) {
     super(props);
     this.addDigit = this.addDigit.bind(this);
@@ -10,19 +19,51 @@ export default class Calculator extends Component {
     this.clearMemory = this.clearMemory.bind(this);
   }
   clearMemory() {
-    console.log("clear");
+    this.setState({ ...initialState });
   }
   addDigit(n) {
-    console.log(n);
+    if (n === "." && this.state.displaValue.includes(".")) {
+      return;
+    }
+    const clearDisplay =
+      this.state.displaValue === "0" || this.state.clearDisplay;
+    const currentValor = clearDisplay ? "" : this.state.displaValue;
+    const displaValue = currentValor + n;
+    this.setState({ displaValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displaValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
   }
-  setOperation(op) {
-    console.log(op);
+  setOperation(operation) {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+      const values = [...this.state.values];
+      let geval = eval;
+      values[0] = geval(`${values[0]} ${currentOperation} ${values[1]}`);
+      values[1] = 0;
+
+      this.setState({
+        displaValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value="100" />
+        <Display value={this.state.displaValue} />
         <Button value="AC" click={this.clearMemory} triple operation="true" />
         <Button value="/" click={this.setOperation} operation="true" />
         <Button value="7" click={this.addDigit} />
